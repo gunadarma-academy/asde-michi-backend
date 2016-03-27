3ae9a5a67a691962ef619136a023337b08222668
Muhammad Patria <muhpatr@gmail.com>
2016-03-27 21:38:59 +0700

    Create twitter auth strategy and login session
    Create twitter auth strategy based on the twitter API configuration in
    dev_config.json, and create the link to start authentication via twitter,
    then save the session using cookie, and show the session.

--- server.js
+++ server.js
@@ -16,12 +16,66 @@ server.register([Bell, Cookie], function (err) {
         throw err;
     }
 	
+	server.auth.strategy('twitter', 'bell', {
+        provider: 'twitter',
+        password: Config.auth.twitter.password,
+        isSecure: false,
+        clientId: Config.auth.twitter.clientId,
+        clientSecret: Config.auth.twitter.clientSecret
+    });
+
+    server.auth.strategy('session', 'cookie', {
+        password: Config.session.cookieOptions.password,
+        cookie: 'sid',
+        redirectTo: '/login',
+        redirectOnTry: false,
+        isSecure: false
+    })
+
+    server.route({
+        method: ['GET', 'POST'],
+        path: '/login',
+        config: {
+            auth: 'twitter',
+            handler: function (request, reply) {
+                var t = request.auth.credentials;
+                console.log('t', t);
+
+                var profile = {
+                    token: t.token,
+                    secret: t.secret,
+                    twitterId: t.profile.id,
+                    twitterName: t.profile.username,
+                    avatar: t.profile.raw.profile_image_url.replace('_normal', ''),
+                    website: t.profile.raw.entities.url.urls[0].expanded_url,
+                    about: t.profile.raw.description,
+                    fullName: t.profile.displayName,
+                };
+                console.log('profile', profile);
+                request.auth.session.clear();
+                request.auth.session.set(profile);
+                return reply.redirect('/');
+
+            }
+        }
+    });
+	
 	server.route({
         method: 'GET',
         path: '/',
         config: {
+			auth: {
+                strategy: 'session',
+                mode: 'try'
+            },
             handler: function (request, reply) {
-                reply('<h1>Hello World!</h1>')
+                if(request.auth.isAuthenticated) {
+                    var profile = request.auth.credentials
+                    reply('<h1>Session</h1><pre>' + JSON.stringify(request.auth.credentials, null, 4) + '</pre>')
+                }
+                else {
+                    reply('<h1><a href="/login">Login Via Twitter</a></h1>')
+                }
             }
         }
     });
