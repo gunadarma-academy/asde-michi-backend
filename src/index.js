eca3a6af1a6584c6fab7c51754ab660e0dd076db
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-18 05:07:44 +0700

    Start to maintain user management properly
    - Create home, signup, and signin page
    - Create signup middleware
    - Redirect success auth to app
    - Restrict the Message service to only authenticated users
    - Not yet able to authenticate using JWT

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const port = app.get('port');
 const server = app.listen(port);
 
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+  console.log(`Feathers app started on ${app.get('host')}:${port}`)
 );
