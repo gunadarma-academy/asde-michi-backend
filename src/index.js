bb066e46cea61c1ff418a89ce419758ce3544a0f
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-24 01:43:13 +0700

    Tweak PUBLIC path and logging

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 'use strict';
 
-// PUBLIC FRONTEND
-// Will be deprecated
-
 const path = require('path');
 const compress = require('compression');
 const cors = require('cors');
 const feathers = require('feathers');
 const configuration = require('feathers-configuration');
+const winston = require('winston');
 const serveStatic = require('feathers').static;
 const favicon = require('serve-favicon');
 
 const app = feathers();
 
 app.configure(configuration(path.join(__dirname, '..')));
+const PUBLIC = path.join(path.join(__dirname, '../public/'));
+winston.info("PUBLIC PATH:", PUBLIC);
 
 // Configure the express
 app.use(compress())
   .options('*', cors())
   .use(cors())
-  .use(favicon(path.join(app.get('public'), 'favicon.ico')))
+  .use(favicon(path.join(PUBLIC, 'favicon.ico')))
+  .get('/signup', function (req, res) {
+    res.sendfile(PUBLIC  + 'signup.html');
+  })
+  .get('/signin', function (req, res) {
+    res.sendfile(PUBLIC + 'signin.html');
+  })
+  .get('/home', function (req, res) {
+    res.sendfile(PUBLIC + 'home.html');
+  })
   .use('/', serveStatic(app.get('public')));
+
 
 const HOST = '127.0.0.1';
 const PORT = 8080;
@@ -32,7 +42,8 @@ const server = app.listen(PORT, HOST, err => {
   }
   const host = server.address().address;
   var port = server.address().port;
-  console.log(`Arlin frontend app running at http://${host}:${port}`);
+  winston.info('Express server Started');
+  winston.info(`Arlin frontend app running at http://${host}:${port}`);
 });
 
 module.exports = app;
