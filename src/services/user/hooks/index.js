6aeb4e42879404313f92e486d56205fea846fcf8
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-25 02:45:16 +0700

    Breakline JSON data

--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -18,25 +18,31 @@ exports.before = {
     auth.restrictToAuthenticated()
   ],
   create: [
-    auth.hashPassword(),
+    auth.hashPassword()
   ],
   update: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' }),
+    auth.restrictToOwner({
+      ownerField: '_id'
+    })
   ],
   patch: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' }),
+    auth.restrictToOwner({
+      ownerField: '_id'
+    })
   ],
   remove: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
+    auth.restrictToOwner({
+      ownerField: '_id'
+    })
   ]
 };
 
